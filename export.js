d5f425e4f9b7cd914878b102c06c8dbc672f032a
LaKeisha <lakeisharprice@gmail.com>
2021-03-12 22:37:53 -0500

    updates

--- export.js
+++ export.js
@@ -3,3 +3,19 @@ function add(num1, num2){
     return res
 }
 add(2,2)
+
+function subtract(num1,num2) {
+    let res = num1-num2
+    return res
+}
+subtract(4,2)
+
+const add = () => {
+return 4
+};
+
+const subtract =() => {
+    return 2
+}
+exports.add=add;
+exports.subtract=subtract;
